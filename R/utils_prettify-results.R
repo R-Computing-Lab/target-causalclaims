95d6086d35f666e7ec0ce2d33b01672ae5172057
Mason Garrison <garrissm@wfu.edu>
2023-03-17 15:08:18 -0400

    Update utils_prettify-results.R

--- R/utils_prettify-results.R
+++ R/utils_prettify-results.R
@@ -7,16 +7,40 @@
 #' @export
 #'
 #' @examples
-prettify_regression_results <- function(regression_object) {
-  regression_object %>%
-    gtsummary::tbl_regression(intercept=TRUE) %>%
-    gtsummary::add_glance_source_note(
-      label = list(statistic ~ "F-statistic",
-                   df  ~ "DF1",
-                   df.residual  ~ "DF2"),
-      include = c(r.squared, statistic, df, df.residual, p.value, nobs)
-      ) %>%
-    gtsummary::modify_header(
-      statistic ~ "**t-statistic**", p.value ~ "**p-value**"
-      )
+prettify_regression_results <- function(regression_object,
+                                        intercept=TRUE,
+                                        standardize=FALSE,
+					digits=3) {
+										
+temp <-  regression_object %>%
+            gtsummary::tbl_regression(intercept=intercept, 
+                                      pvalue_fun = ~ gtsummary::style_pvalue(.x, digits = digits),
+                                      estimate_fun = ~ gtsummary::style_sigfig(.x, digits = digits)
+            ) %>%
+        gtsummary::modify_header(
+            statistic ~ "**t-statistic**", p.value ~ "**p-value**"
+        ) %>%
+        gtsummary::add_glance_source_note(
+            label = list(statistic ~ "F-statistic",
+                         df  ~ "DF1",
+                         df.residual  ~ "DF2"),
+            include = c(r.squared, statistic, df, df.residual, p.value, nobs)
+        )
+if(standardize){
+temp_stnd <-      regression_object %>%
+        gtsummary::tbl_regression(intercept=intercept, 
+                                  estimate_fun = ~ gtsummary::style_sigfig(.x, digits = digits),
+                                  pvalue_fun = ~ gtsummary::style_pvalue(.x, digits = digits),
+                                  tidy_fun = gtsummary::tidy_standardize,
+								  conf.int=FALSE) %>%
+        gtsummary::modify_header(
+            estimate ~ "**β**"
+        ) 
+temp <-
+  tbl_merge(
+    list(temp_stnd, temp),
+    tab_spanner = c("**Standardized**", "**Unstandardized**")
+  )
+}
+temp  
 }
